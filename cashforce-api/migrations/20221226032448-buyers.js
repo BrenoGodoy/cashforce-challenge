3365908cf432c4575b21765f4a25f4421360af26
Breno Godoy <godoyy25z@gmail.com>
2022-12-26 00:53:47 -0300

    feat: cria migration e model da tabela buyers

---
+++ cashforce-api/migrations/20221226032448-buyers.js
@@ -0,0 +1,106 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('buyers', {
+      id: {
+        allowNull: false,
+        // primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      tradingName: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      cashforceTax: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      responsibleName: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      responsibleEmail: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      responsiblePosition: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      responsiblePhone: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      responsibleMobile: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      website: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      postalCode: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      address: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      number: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      complement: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      neighborhood: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      city: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      state: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      phoneNumber: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      situation: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      situationDate: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      cnpjId: {
+        type: Sequelize.INTEGER,
+        defaultValue: null
+      },
+      confirm: {
+        type: Sequelize.INTEGER,
+        defaultValue: 1
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('buyers');
+  }
+};
---
+++ cashforce-api/models/buyers.js
@@ -0,0 +1,115 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class buyers extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  buyers.init({
+    id: {
+      allowNull: false,
+      // primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    tradingName: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    cashforceTax: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    responsibleName: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    responsibleEmail: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    responsiblePosition: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    responsiblePhone: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    responsibleMobile: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    website: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    postalCode: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    address: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    number: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    complement: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    neighborhood: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    city: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    state: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    situation: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    situationDate: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    },
+    cnpjId: {
+      type: DataTypes.INTEGER,
+      defaultValue: null
+    },
+    confirm: {
+      type: DataTypes.INTEGER,
+      defaultValue: 1
+    },
+    email: {
+      type: DataTypes.STRING,
+      defaultValue: null
+    }
+  }, {
+    sequelize,
+    modelName: 'buyers',
+  });
+  return buyers;
+};
