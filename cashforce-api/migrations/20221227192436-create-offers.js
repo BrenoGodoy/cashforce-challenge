c154369f916d40222043c94c9dc1ce2bcf353e79
Breno Godoy <godoyy25z@gmail.com>
2022-12-27 16:26:07 -0300

    feat: Cria migration e model da tabela offers

---
+++ cashforce-api/migrations/20221227192436-create-offers.js
@@ -0,0 +1,58 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('offers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      tax: {
+        type: Sequelize.STRING
+      },
+      tariff: {
+        type: Sequelize.STRING
+      },
+      adValorem: {
+        type: Sequelize.STRING
+      },
+      float: {
+        type: Sequelize.STRING
+      },
+      iof: {
+        type: Sequelize.STRING
+      },
+      expiresIn: {
+        type: Sequelize.DATE
+      },
+      paymentStatusSponsor: {
+        type: Sequelize.INTEGER
+      },
+      paymentStatusProvider: {
+        type: Sequelize.INTEGER
+      },
+      orderId: {
+        type: Sequelize.INTEGER
+      },
+      sponsorId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('offers');
+  }
+};
