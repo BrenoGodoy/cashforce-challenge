997ab6924fd6ea3cac4e44cd2527cb6ea6b31cb3
Breno Godoy <godoyy25z@gmail.com>
2022-12-27 16:34:24 -0300

    feat: Cria migration e model para tabela orderportions

---
+++ cashforce-api/migrations/20221227192927-create-orderportions.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('orderportions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      nDup: {
+        type: Sequelize.STRING
+      },
+      dVenc: {
+        type: Sequelize.STRING
+      },
+      vDup: {
+        type: Sequelize.STRING
+      },
+      availableToMarket: {
+        type: Sequelize.INTEGER
+      },
+      orderId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('orderportions');
+  }
+};
