a2c9825639be05bd9a386d2ef273e0e3918989ec
Breno Godoy <godoyy25z@gmail.com>
2022-12-28 16:36:11 -0300

    fix: Retira ids extras adicionados via sequelize-cli

--- cashforce-api/migrations/20221227191621-create-cnpjs.js
+++ cashforce-api/migrations/20221227191621-create-cnpjs.js
@@ -9,9 +9,6 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      id: {
-        type: Sequelize.INTEGER
-      },
       cnpj: {
         type: Sequelize.STRING
       },
