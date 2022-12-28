1565247b14497665c7cb08a45ee023fc23718bb6
Breno Godoy <godoyy25z@gmail.com>
2022-12-28 01:40:23 -0300

    feat: Cria seeder para tabela buyers

--- cashforce-api/seeders/20221228042439-buyers.js
+++ cashforce-api/seeders/20221228042439-buyers.js
@@ -3,23 +3,37 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    
+    await queryInterface.bulkInsert('buyers', [{
+      id: 1,
+      name: 'SACADO 001',
+      tradingName: 'SACADO 001 LTDA',
+      cashforceTax: 0,
+      // responsibleName: ,
+      // responsibleEmail: ,
+      // responsiblePosition: ,
+      // responsiblePhone: ,
+      // responsibleMobile: ,
+      // website: ,
+      // postalCode: ,
+      // address: ,
+      // number: ,
+      // complement: ,
+      // neighborhood:,
+      // city: ,
+      // state: ,
+      // phoneNumber: ,
+      // situation: ,
+      // situationDate: ,
+      createdAt: '2020-10-29 21:20:33',
+      updatedAt: '2020-10-29 21:20:34',
+      cnpjId: 1,
+      confirm: 1,
+      // email:,
+    }], {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('buyers', null, {}); 
   }
 };
