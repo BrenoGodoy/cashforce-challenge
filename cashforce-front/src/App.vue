c42d9c0abec9397052da1893613f949878d287a5
Breno Godoy <godoyy25z@gmail.com>
2023-01-02 18:42:32 -0300

    feat: cria header da tabela

--- cashforce-front/src/App.vue
+++ cashforce-front/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
   import SideBar from './components/SideBar.vue';
+  import tableHeaderVue from './components/tableHeader.vue';
 </script>
 
 <template>
   <SideBar />
+  <tableHeaderVue />
 </template>
 
 <style scoped>
---
+++ cashforce-front/src/components/tableHeader.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+<template>
+  <div class="table-header">
+    <img src="../assets/header.png" />
+  </div>
+</template>
+
+<style scoped>
+.table-header {
+  /* Auto layout */
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 9px;
+
+  position: absolute;
+  width: 255px;
+  height: 53px;
+  left: 336px;
+  top: 104px;
+}
+</style>
